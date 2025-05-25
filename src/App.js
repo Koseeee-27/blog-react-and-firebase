a37550a16b6e50b22b93cd4f5107f339164c32f0
Koseeee-27 <kosekose2741@gmail.com>
2025-05-26 00:38:38 +0900

    ルーティング設定

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import Home from './components/Home/Home';
+import Login from './components/Auth/Login';
+import Logout from './components/Auth/Logout';
+import CreatePost from './components/CreatePost/CreatePost'
 
 function App() {
   return (
-    <div >
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} ></Route>
+        <Route path='/createpost' element={<CreatePost />} ></Route>
+        <Route path='/login' element={<Login />} ></Route>
+        <Route path='/logout' element={<Logout />} ></Route>
+      </Routes>
+    </Router>
   );
 }
 
