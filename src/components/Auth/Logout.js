f489872898a47bbce056af5e7e14e890cf15da4f
Koseeee-27 <kosekose2741@gmail.com>
2025-05-26 13:16:57 +0900

    ログアウト基本の機能完成

--- src/components/Auth/Logout.js
+++ src/components/Auth/Logout.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { auth, provider } from '../../firebase';
+import { useNavigate } from 'react-router-dom';
+import { signOut } from 'firebase/auth';
 
-const Logout = () => {
+
+const Logout = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+  const logout = () => {
+    // Googleログアウトの処理をここに実装
+      signOut(auth).then(() => {
+        localStorage.clear();
+        setIsAuth(false);
+        navigate('/login');
+      })
+  };
+
   return (
-    <div>Logout</div>
-  )
-}
+    <div>
+      <p>ログアウトする</p>
+      <button onClick={logout}>Googleでログアウト</button>
+    </div>
+  );
+};
 
-export default Logout
+export default Logout;
