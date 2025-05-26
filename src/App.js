6102809873df62430f8202820c71d37a6a019ee2
Koseeee-27 <kosekose2741@gmail.com>
2025-05-26 13:03:27 +0900

    ログイン機能

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 import Home from './components/Home/Home';
 import Login from './components/Auth/Login';
@@ -7,13 +8,16 @@ import CreatePost from './components/CreatePost/CreatePost'
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
+  // ログイン状態
+  const [isAuth, setIsAuth] = useState(false);
+
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} ></Route>
         <Route path='/createpost' element={<CreatePost />} ></Route>
-        <Route path='/login' element={<Login />} ></Route>
+        <Route path='/login' element={<Login setIsAuth={setIsAuth} />} ></Route>
         <Route path='/logout' element={<Logout />} ></Route>
       </Routes>
     </Router>
