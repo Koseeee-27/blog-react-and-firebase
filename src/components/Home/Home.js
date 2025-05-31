bd467b07c74463399c160c854c89f4c4007f2d02
Koseeee-27 <kosekose2741@gmail.com>
2025-05-31 15:19:39 +0900

    仮のホームページ作成

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import './Home.css'; 
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='homePage'>
+      <div className='postContainer'>
+        <div className='postHeader'>
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+          今はReactの学習中です。これから頑張ってエンジニアとして成長していきたいと思います。
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>@Kosei</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
