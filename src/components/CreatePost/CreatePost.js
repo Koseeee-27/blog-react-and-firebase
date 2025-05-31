7fc71b7623e98bd2e4a529d1676ac678952f2d31
Koseeee-27 <kosekose2741@gmail.com>
2025-05-31 14:47:31 +0900

    投稿機能作成

--- src/components/CreatePost/CreatePost.js
+++ src/components/CreatePost/CreatePost.js
@@ -1,21 +1,50 @@
-import React from 'react'
-import './CreatePost.css'
+import React, { useState } from "react";
+import "./CreatePost.css";
+import { collection, addDoc } from "firebase/firestore"; 
+import { db, auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
 
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [postText, setPostText] = useState("");
+
+  const navigate = useNavigate();
+
+  const createPost = async () =>  {
+    await addDoc(collection(db, "postData"), {
+      title: title,
+      postText: postText,
+      author: {
+        username: auth.currentUser.displayName,
+        id: auth.currentUser.uid,
+      }
+    })
+    .then(() => {
+      navigate("/");
+    })
+  }
+
   return (
-    <div className='createPostPage'>
-      <div className='postContainer'>
+    <div className="createPostPage">
+      <div className="postContainer">
         <h1>記事を投稿する</h1>
         <div>タイトル</div>
-        <input type='text' placeholder='タイトルを記入' />
-        <div className='inputPost'>
+        <input
+          type="text"
+          placeholder="タイトルを記入"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <div className="inputPost">
           <div>投稿</div>
-          <textarea placeholder='投稿内容を記入'></textarea>
+          <textarea
+            placeholder="投稿内容を記入"
+            onChange={(e) => setPostText(e.target.value)}
+          ></textarea>
         </div>
-        <button className='postButton'>投稿する</button>
+        <button className="postButton" onClick={createPost}>投稿する</button>
       </div>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default CreatePost
+export default CreatePost;
